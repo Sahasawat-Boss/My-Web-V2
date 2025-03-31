43a84f547c9db0fc9c510444ddc0accc4bb3c72e
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-31 14:58:13 +0700

    Update Mobile Res of ResMockup Comps

--- src/app/components/details-comps/ResponsiveMockup.tsx
+++ src/app/components/details-comps/ResponsiveMockup.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 export default function ResponsiveMockup() {
     return (
         <section className="w-full flex justify-center py-10 px-4 shadow">
-            <div className="relative w-[450px]">
+            <div className="relative w-[380px]">
                 {/* Laptop Mockup */}
                 <Image
                     src="/Images/res/res02.png"
                     alt="Laptop Mockup"
                     width={1385}
                     height={788}
-                    className="w-full h-auto rounded-xl shadow-lg"
+                    className="w-full h-[110%] sm:h-full rounded-xl shadow-lg"
                     priority
                 />
 
                 {/* Mobile Mockup - Positioned relative to the laptop */}
-                <div className="absolute right-[35px] bottom-[-8px] w-[95px]">
+                <div className="absolute right-[13px] sm:right-[25px] bottom-[-20px] sm:bottom-[-6px] w-[80px]">
                     <Image
                         src="/Images/res/res01.png"
                         alt="Mobile Mockup"
